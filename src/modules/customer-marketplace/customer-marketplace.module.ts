69c9c83d3a9509183d5f9519fde36608e5a82c22
Jarvis (Dcodax) <jarvis@dcodax.com>
2026-05-08 07:14:02 +0000

    fix(customer): import jwt module

--- src/modules/customer-marketplace/customer-marketplace.module.ts
+++ src/modules/customer-marketplace/customer-marketplace.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from '../../database/prisma.service';
 import { CustomerMarketplaceController } from './customer-marketplace.controller';
 import { CustomerMarketplaceService } from './customer-marketplace.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [CustomerMarketplaceController],
   providers: [CustomerMarketplaceService, PrismaService],
 })
