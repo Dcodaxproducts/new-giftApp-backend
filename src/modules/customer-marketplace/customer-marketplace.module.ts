add08d53a4256f89b22294de6e3cefee09506c52
Jarvis (Dcodax) <jarvis@dcodax.com>
2026-05-08 06:42:16 +0000

    fix(customer): register prisma provider

--- src/modules/customer-marketplace/customer-marketplace.module.ts
+++ src/modules/customer-marketplace/customer-marketplace.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../../database/prisma.service';
 import { CustomerMarketplaceController } from './customer-marketplace.controller';
 import { CustomerMarketplaceService } from './customer-marketplace.service';
 
 @Module({
   controllers: [CustomerMarketplaceController],
-  providers: [CustomerMarketplaceService],
+  providers: [CustomerMarketplaceService, PrismaService],
 })
 export class CustomerMarketplaceModule {}
