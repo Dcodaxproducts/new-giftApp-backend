64bb47aac901c999d91113a984f13eb8a185a74c
Jarvis (Dcodax) <jarvis@dcodax.com>
2026-05-20 05:01:58 +0000

    fix(reporting): import database module

--- src/modules/reporting-core/reporting-core.module.ts
+++ src/modules/reporting-core/reporting-core.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { AuditLogWriterRepository } from '../../common/repositories/audit-log-writer.repository';
 import { AuditLogWriterService } from '../../common/services/audit-log.service';
+import { DatabaseModule } from '../../database/database.module';
 import { BroadcastNotificationsModule } from '../broadcast-notifications/broadcast-notifications.module';
 import { ReportAuditService } from './report-audit.service';
 import { ReportEvidencePolicyService } from './report-evidence-policy.service';
@@ -11,7 +12,7 @@ import { ReportingCoreService } from './reporting-core.service';
 
 @Global()
 @Module({
-  imports: [BroadcastNotificationsModule],
+  imports: [DatabaseModule, BroadcastNotificationsModule],
   providers: [AuditLogWriterRepository, AuditLogWriterService, ReportAuditService, ReportEvidencePolicyService, ReportNotificationService, ReportStatusPolicyService, ReportingCoreRepository, ReportingCoreService],
   exports: [ReportingCoreService, ReportAuditService, ReportEvidencePolicyService, ReportNotificationService, ReportStatusPolicyService],
 })
