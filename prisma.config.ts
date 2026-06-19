d7f34927d239f0a921b560a88d3a7112982a1ad6
umerwe <uemyy1@gmail.com>
2026-06-19 14:06:09 +0500

    added console for testing

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,11 @@
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
+import 'dotenv/config'; 
+import { defineConfig } from 'prisma/config'; 
 
-export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  datasource: {
-    url:
-      process.env.DATABASE_URL!
-  },
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
+export default defineConfig({ 
+  schema: 'prisma/schema.prisma', 
+  datasource: { 
+    url: process.env.DATABASE_URL! 
+  }, 
 });
