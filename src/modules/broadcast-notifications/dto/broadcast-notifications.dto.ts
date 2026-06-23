1cde4c6aec0129c7e4308978842cebdfc055b660
umerwe <uemyy1@gmail.com>
2026-06-23 12:59:54 +0500

    fix: update validation for audience property in CreateBroadcastDto

--- src/modules/broadcast-notifications/dto/broadcast-notifications.dto.ts
+++ src/modules/broadcast-notifications/dto/broadcast-notifications.dto.ts
@@ -1,10 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsIn, IsString, MaxLength, MinLength } from 'class-validator';
 
 export enum BroadcastAudienceDto { ALL_USERS = 'ALL_USERS', PROVIDER = 'PROVIDER', USER = 'USER' }
 
 export class CreateBroadcastDto {
   @ApiProperty({ example: 'Maintenance Notice', maxLength: 50 }) @IsString() @MinLength(1) @MaxLength(50) title!: string;
   @ApiProperty({ example: 'Type your message here...' }) @IsString() @MinLength(1) message!: string;
-  @ApiProperty({ enum: BroadcastAudienceDto, example: BroadcastAudienceDto.ALL_USERS }) @IsEnum(BroadcastAudienceDto) audience!: BroadcastAudienceDto;
+  @ApiProperty({ enum: BroadcastAudienceDto, example: BroadcastAudienceDto.ALL_USERS }) @IsIn(Object.values(BroadcastAudienceDto)) audience!: BroadcastAudienceDto;
 }
